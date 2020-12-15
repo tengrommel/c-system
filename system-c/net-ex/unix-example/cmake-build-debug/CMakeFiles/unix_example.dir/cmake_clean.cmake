a2e1df7c5e3d09add7e108ec0633693382f86ede
teng <wwwsousoucom@126.com>
2020-12-15 17:43:23 +0800

    net adapter

---
+++ system-c/net-ex/unix-example/cmake-build-debug/CMakeFiles/unix_example.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/unix_example.dir/main.c.o"
+  "unix_example"
+  "unix_example.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/unix_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
